721e67b0ce5a3a4ec435d1120fff3f0034d288a9
Jakub Pastuszek <jpastuszek@gmail.com>
2016-12-05 11:31:41 +0000

    inital test and parsing (broken) full syslog frame

--- src/input/syslog.rs
+++ src/input/syslog.rs
@@ -11,9 +11,10 @@ use futures::sync::mpsc;
 pub struct SyslogMessage {
     pub facility: u8,
     pub severity: u8,
+    pub message: String
 }
 
-fn str(bytes: &[u8]) -> Result<&str, Utf8Error> {
+fn string(bytes: &[u8]) -> Result<&str, Utf8Error> {
     from_utf8(bytes)
 }
 
@@ -23,7 +24,7 @@ enum InputIntError {
 }
 
 fn number_u8(bytes: &[u8]) -> Result<u8, InputIntError> {
-    str(bytes).map_err(InputIntError::Utf8Error)
+    string(bytes).map_err(InputIntError::Utf8Error)
         .and_then(|s| s.parse().map_err(InputIntError::ParseIntError))
 }
 
@@ -32,14 +33,27 @@ fn number_u8(bytes: &[u8]) -> Result<u8, InputIntError> {
 named!(syslog_parser<&[u8], SyslogMessage>, chain!(
     priority: delimited!(tag!(b"<"),
                          map_res!(take_until!(&b">"[..]), number_u8),
-                         tag!(b">")),
+                         tag!(b">")) ~
+    message: terminated!(map_res!(take_until!(&b"\n"[..]), string), tag!(b"\n")),
     || SyslogMessage {
         facility: priority >> 3,
-        severity: priority - (priority >> 3 << 3)
+        severity: priority - (priority >> 3 << 3),
+        message: message.to_owned()
     }));
 
 pub fn tcp_syslog_input(handle: Handle, addr: &SocketAddr) -> mpsc::Receiver<SyslogMessage> {
     tcp_nom_input("syslog", handle, addr, syslog_parser)
 }
 
+#[cfg(test)]
+mod parser_tests {
+    use super::syslog_parser;
 
+    #[test]
+    fn priority() {
+        let (i, o) = syslog_parser(b"<165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog - foobar\n").unwrap();
+        assert_eq!(i, b"");
+        assert_eq!(o.facility, 20);
+        assert_eq!(o.severity, 5);
+    }
+}
