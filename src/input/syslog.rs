e10f79ca65dcbd6c5c02a80cd7ebcddaa74308ca
Jakub Pastuszek <jpastuszek@gmail.com>
2016-12-06 13:44:41 +0000

    RFC5425 syslog framing

--- src/input/syslog.rs
+++ src/input/syslog.rs
@@ -56,7 +56,7 @@ named!(message<&[u8], Option<&str> >, return_error!(ErrorKind::Custom(99), do_pa
         (ret)
     )));
 
-named!(syslog_parser_rfc5424<&[u8], SyslogMessage>, do_parse!(
+named!(pub syslog_rfc5424<&[u8], SyslogMessage>, do_parse!(
     priority: priority >>
     timestamp: timestamp >>
     hostname: hostname >>
@@ -76,38 +76,70 @@ named!(syslog_parser_rfc5424<&[u8], SyslogMessage>, do_parse!(
         message: message.map(|s| s.to_owned())
     })));
 
+named!(pub syslog_rfc5425_frame<&[u8], &[u8]>, do_parse!(
+        msg_len: return_error!(ErrorKind::Custom(1),
+           terminated!(map_res!(take_until!(&b" "[..]), parse::int_u8), tag!(" "))) >>
+        syslog_msg: take!(msg_len) >>
+        (syslog_msg)
+    ));
+
+named!(pub syslog_rfc5424_in_rfc5425_frame<&[u8], SyslogMessage>,
+       flat_map!(call!(syslog_rfc5425_frame), call!(syslog_rfc5424)));
+
 pub mod simple_errors {
-    use super::*;
+    use super::SyslogMessage;
     use nom::{IResult, ErrorKind};
 
-    pub fn syslog_parser_rfc5424(input: &[u8]) -> IResult<&[u8], SyslogMessage, &'static str> {
-        super::syslog_parser_rfc5424(input).map_err(|err| ErrorKind::Custom(match err {
-                ErrorKind::Custom(1) => "Bad syslog priority tag format",
-                ErrorKind::Custom(2) => "Unrecognized syslog timestamp format",
-                ErrorKind::Custom(3) => "Expected syslog hostname",
-                ErrorKind::Custom(4) => "Expected syslog application name",
-                ErrorKind::Custom(5) => "Expected syslog process ID",
-                ErrorKind::Custom(6) => "Expected syslog message ID",
-                ErrorKind::Custom(7) => "Expected valid syslog structured data",
-                ErrorKind::Custom(99) => "Bad syslog message payload",
-                _ => "Syslog parser did not match"
+    pub fn syslog_rfc5424(input: &[u8]) -> IResult<&[u8], SyslogMessage, &'static str> {
+        super::syslog_rfc5424(input).map_err(|err| ErrorKind::Custom(match err {
+            ErrorKind::Custom(1) => "Bad syslog priority tag format",
+            ErrorKind::Custom(2) => "Unrecognized syslog timestamp format",
+            ErrorKind::Custom(3) => "Expected syslog hostname",
+            ErrorKind::Custom(4) => "Expected syslog application name",
+            ErrorKind::Custom(5) => "Expected syslog process ID",
+            ErrorKind::Custom(6) => "Expected syslog message ID",
+            ErrorKind::Custom(7) => "Expected valid syslog structured data",
+            ErrorKind::Custom(99) => "Bad syslog message payload",
+            _ => "Syslog parser did not match"
         }))
     }
+
+    pub fn syslog_rfc5425_frame(input: &[u8]) -> IResult<&[u8], &[u8], &'static str> {
+        super::syslog_rfc5425_frame(input).map_err(|err| ErrorKind::Custom(match err {
+            ErrorKind::Custom(1) => "Expected syslog RFC5425 frame message length",
+            _ => "Syslog RFC5425 frame parser did not match"
+        }))
+    }
+
+    named!(pub syslog_rfc5424_in_rfc5425_frame<&[u8], SyslogMessage, &'static str>,
+           flat_map!(call!(syslog_rfc5425_frame), call!(syslog_rfc5424)));
 }
 
 pub fn tcp_syslog_input(handle: Handle, addr: &SocketAddr) -> mpsc::Receiver<SyslogMessage> {
-    tcp_nom_input("syslog", handle, addr, simple_errors::syslog_parser_rfc5424)
+    tcp_nom_input("syslog", handle, addr, simple_errors::syslog_rfc5424_in_rfc5425_frame)
 }
 
 #[cfg(test)]
-mod syslog_rfc5424_test {
-    use super::simple_errors::syslog_parser_rfc5424;
+mod syslog_rfc5425_frame_tests {
+    use super::simple_errors::syslog_rfc5425_frame;
+
+    #[test]
+    fn length() {
+        let (i, o) = syslog_rfc5425_frame(b"79 <165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 - foo\nbarEOF").unwrap();
+        assert_eq!(o, &b"<165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 - foo\nbar"[..]);
+        assert_eq!(i, &b"EOF"[..]);
+    }
+}
+
+#[cfg(test)]
+mod syslog_rfc5424_tests {
+    use super::simple_errors::syslog_rfc5424;
     use super::Timestamp;
     use nom::ErrorKind;
 
     #[test]
     fn priority() {
-        let (i, o) = syslog_parser_rfc5424(b"<165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 - foobar\n").unwrap();
+        let (i, o) = syslog_rfc5424(b"<165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 - foobar\n").unwrap();
         assert!(i.is_empty());
         assert_eq!(o.facility, 20);
         assert_eq!(o.severity, 5);
@@ -115,72 +147,72 @@ mod syslog_rfc5424_test {
 
     #[test]
     fn priority_error() {
-        let err = syslog_parser_rfc5424(b"<16x5>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 - foobar\n").unwrap_err();
+        let err = syslog_rfc5424(b"<16x5>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 - foobar\n").unwrap_err();
         assert_matches!(err, ErrorKind::Custom("Bad syslog priority tag format"));
     }
 
     #[test]
     fn timestamp() {
-        let (i, o) = syslog_parser_rfc5424(b"<165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 - foobar\n").unwrap();
+        let (i, o) = syslog_rfc5424(b"<165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 - foobar\n").unwrap();
         assert!(i.is_empty());
         assert_eq!(o.timestamp, Timestamp::parse_from_rfc3339("2003-10-11T22:14:15.003Z").unwrap());
     }
 
     #[test]
     fn timestamp_error() {
-        let err = syslog_parser_rfc5424(b"<165>1 XXXX-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 - foobar\n").unwrap_err();
+        let err = syslog_rfc5424(b"<165>1 XXXX-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 - foobar\n").unwrap_err();
         assert_matches!(err, ErrorKind::Custom("Unrecognized syslog timestamp format"));
     }
 
     #[test]
     fn hostname() {
-        let (i, o) = syslog_parser_rfc5424(b"<165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 - foobar\n").unwrap();
+        let (i, o) = syslog_rfc5424(b"<165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 - foobar\n").unwrap();
         assert!(i.is_empty());
         assert_eq!(o.hostname, "mymachine.example.com");
     }
 
     #[test]
     fn hostname_error() {
-        let err = syslog_parser_rfc5424(b"<165>1 2003-10-11T22:14:15.003Z \n").unwrap_err();
+        let err = syslog_rfc5424(b"<165>1 2003-10-11T22:14:15.003Z \n").unwrap_err();
         assert_matches!(err, ErrorKind::Custom("Expected syslog hostname"));
     }
 
     #[test]
     fn hostname_nil_error() {
-        let err = syslog_parser_rfc5424(b"<165>1 2003-10-11T22:14:15.003Z - \n").unwrap_err();
+        let err = syslog_rfc5424(b"<165>1 2003-10-11T22:14:15.003Z - \n").unwrap_err();
         assert_matches!(err, ErrorKind::Custom("Expected syslog hostname"));
     }
 
     #[test]
     fn app_name() {
-        let (i, o) = syslog_parser_rfc5424(b"<165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 - foobar\n").unwrap();
+        let (i, o) = syslog_rfc5424(b"<165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 - foobar\n").unwrap();
         assert!(i.is_empty());
         assert_eq!(o.app_name, Some("evntslog".to_owned()));
     }
 
     #[test]
     fn app_name_none() {
-        let (i, o) = syslog_parser_rfc5424(b"<165>1 2003-10-11T22:14:15.003Z mymachine.example.com - - ID47 - foobar\n").unwrap();
+        let (i, o) = syslog_rfc5424(b"<165>1 2003-10-11T22:14:15.003Z mymachine.example.com - - ID47 - foobar\n").unwrap();
         assert!(i.is_empty());
         assert_eq!(o.app_name, None);
     }
 
     #[test]
     fn app_name_error() {
-        let err = syslog_parser_rfc5424(b"<165>1 2003-10-11T22:14:15.003Z mymachine.example.com \n").unwrap_err();
+        let err = syslog_rfc5424(b"<165>1 2003-10-11T22:14:15.003Z mymachine.example.com \n").unwrap_err();
         assert_matches!(err, ErrorKind::Custom("Expected syslog application name"));
     }
 
     #[test]
     fn message() {
-        let (i, o) = syslog_parser_rfc5424(b"<165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 - foo\nbar").unwrap();
+        let (i, o) = syslog_rfc5424(b"<165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 - foo\nbar").unwrap();
         assert!(i.is_empty());
         assert_eq!(o.message, Some("foo\nbar".to_owned()));
     }
 
     #[test]
     fn message_error() {
-        let err = syslog_parser_rfc5424(b"<165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 - baz\xc3\x28").unwrap_err();
+        let err = syslog_rfc5424(b"<165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 - baz\xc3\x28").unwrap_err();
         assert_matches!(err, ErrorKind::Custom("Bad syslog message payload"));
     }
 }
