8f8b3fe1fd0f34e467d95ac3b16eb364a0a67dd9
Jakub Pastuszek <jpastuszek@gmail.com>
2016-12-15 11:58:04 +0000

    missing changes

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,13 @@
-extern crate futures;
-extern crate tokio_core;
-#[macro_use]
-extern crate nom;
-#[macro_use]
-extern crate error_chain;
-#[macro_use]
-extern crate assert_matches;
-extern crate chrono;
-extern crate maybe_string;
-extern crate uuid;
-extern crate serde;
-extern crate serde_json;
+extern crate logslash;
 
-mod input;
-mod output;
-mod event;
-
-use tokio_core::reactor::Core;
-
-use input::syslog::tcp_syslog_input;
-use output::debug::print_logstash;
+use logslash::event_loop;
+use logslash::input::syslog::tcp_syslog_input;
+use logslash::output::debug::print_logstash;
 
 fn main() {
     println!("Hello, world!");
 
-    let mut event_loop = Core::new().unwrap();
+    let mut event_loop = event_loop();
     let handle = event_loop.handle();
 
     let syslog = tcp_syslog_input(handle, &"127.0.0.1:5514".parse().unwrap());
