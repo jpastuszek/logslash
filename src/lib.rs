c56d7d494cc887ffe499b57b436c644880cd40a3
Jakub Pastuszek <jpastuszek@gmail.com>
2016-12-15 11:57:19 +0000

    split off lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+extern crate futures;
+extern crate tokio_core;
+#[macro_use]
+extern crate nom;
+#[macro_use]
+extern crate error_chain;
+#[macro_use]
+extern crate assert_matches;
+extern crate chrono;
+extern crate maybe_string;
+extern crate uuid;
+extern crate serde;
+extern crate serde_json;
+
+pub mod input;
+pub mod output;
+pub mod event;
+
+use tokio_core::reactor::Core;
+
+pub fn event_loop() -> Core {
+    Core::new().expect("Tokio Core event loop")
+}
