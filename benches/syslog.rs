54bb28587a695ecc7c9f20e17687887d88a1a7f9
Jakub Pastuszek <jpastuszek@gmail.com>
2017-02-07 15:49:00 +0000

    inital working bench of Syslog Codec

--- benches/syslog.rs
+++ benches/syslog.rs
@@ -1,36 +1,64 @@
 #[macro_use]
 extern crate bencher;
 extern crate logslash;
+extern crate futures;
+extern crate tokio_core;
+extern crate tokio_vec_io;
 
 use bencher::Bencher;
+use tokio_vec_io::BufStream;
+use tokio_core::io::Io;
+use futures::Future;
 
-use logslash::event_loop;
+use logslash::codec::syslog::SyslogCodec;
+use logslash::codec::IntoCodec;
+use futures::stream::Stream;
 
-static syslog_rfc5424_newline_examples: &'static str = r#"
+static SYSLOG_RFC5424_NEWLINE_EXAMPLES: &'static str =
+r#"<165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 [exampleSDID@32473 iut="3" eventSource="Application" eventID="1011"][examplePriority@32473 class="high"] foo
+<165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 [exampleSDID@32473 iut="3" eventSource="Application" eventID="1011"][examplePriority@32473 class="high"] foo#012bar
+<165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog 123 - [exampleSDID@32473 iut="3" eventSource="Application" eventID="1011"][examplePriority@32473 class="high"] foo
+<165>1 2003-10-11T22:14:15.003Z mymachine.example.com - 123 - [exampleSDID@32473 iut="3" eventSource="Application" eventID="1011"][examplePriority@32473 class="high"] foo
+<165>1 2003-10-11T22:14:15.003Z mymachine.example.com - 123 - [exampleSDID@32473 iut="3" eventSource="Application" eventID="1011"][examplePriority@32473 class="high"] At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum
 <165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 [exampleSDID@32473 iut="3" eventSource="Application" eventID="1011"][examplePriority@32473 class="high"] foo
 <165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 [exampleSDID@32473 iut="3" eventSource="Application" eventID="1011"][examplePriority@32473 class="high"] foo#012bar
 <165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog 123 - [exampleSDID@32473 iut="3" eventSource="Application" eventID="1011"][examplePriority@32473 class="high"] foo
-<165?1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog 123 - [exampleSDID@32473 iut="3" eventSource="Application" eventID="1011"][examplePriority@32473 class="high"] foo
 <165>1 2003-10-11T22:14:15.003Z mymachine.example.com - 123 - [exampleSDID@32473 iut="3" eventSource="Application" eventID="1011"][examplePriority@32473 class="high"] foo
+<165>1 2003-10-11T22:14:15.003Z mymachine.example.com - 123 - [exampleSDID@32473 iut="3" eventSource="Application" eventID="1011"][examplePriority@32473 class="high"] At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum
 "#;
 
-fn syslog_rfc5424_newline(bench: &mut Bencher) {
-    let mut event_loop = event_loop();
-    let handle = event_loop.handle();
+static SYSLOG_RFC5424_NEWLINE_NO_META_EXAMPLES: &'static str =
+r#"<165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 - foo
+<165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 - foo#012bar
+<165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog 123 - - foo
+<165>1 2003-10-11T22:14:15.003Z mymachine.example.com - 123 - - foo
+<165>1 2003-10-11T22:14:15.003Z mymachine.example.com - 123 - - At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum
+<165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 - foo
+<165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog - ID47 - foo#012bar
+<165>1 2003-10-11T22:14:15.003Z mymachine.example.com evntslog 123 - - foo
+<165>1 2003-10-11T22:14:15.003Z mymachine.example.com - 123 - - foo
+<165>1 2003-10-11T22:14:15.003Z mymachine.example.com - 123 - - At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum
+"#;
 
-    // I need IO here so wI can test the Codec on it
-    // Only TcpStream implements it
-    // But if I have PoolEvented<E> where E: Read + Write then I get it implemented as well on it
-    // E has to be Evented (from mio) and is implemeted for EventedFd (unix file discriptiors)
-    // and mio::channel::Receiver but you cannot Read + Write from it
-    // There is a crate for Unix sockets as well but  nothing for plain files unless we use
-    // EventedFd
-    // I could also impement IO on a Cursor and use it as input and output
-
+fn syslog_rfc5424_newline_x10(bench: &mut Bencher) {
+    let mut buf = Vec::from(SYSLOG_RFC5424_NEWLINE_EXAMPLES);
     bench.iter(|| {
-
+        let input = BufStream::new(buf.as_mut_slice());
+        let output = input.framed(SyslogCodec::rfc5424_in_newline_frame().into_codec()).collect();
+        assert_eq!(output.wait().expect("Ok result").len(), 10);
     })
 }
 
-benchmark_group!(benches, syslog_rfc5424_newline);
+fn syslog_rfc5424_newline_no_meta_x10(bench: &mut Bencher) {
+    let mut buf = Vec::from(SYSLOG_RFC5424_NEWLINE_NO_META_EXAMPLES);
+    bench.iter(|| {
+        let input = BufStream::new(buf.as_mut_slice());
+        let output = input.framed(SyslogCodec::rfc5424_in_newline_frame().into_codec()).collect();
+        assert_eq!(output.wait().expect("Ok result").len(), 10);
+    })
+}
+
+benchmark_group!(benches,
+                 syslog_rfc5424_newline_x10,
+                 syslog_rfc5424_newline_no_meta_x10);
 benchmark_main!(benches);
